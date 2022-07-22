1ff0de46551263d27a3bf63b71351bbe9395a51a
Kod4dev <arthureichenberg8@gmail.com>
2022-07-22 20:48:32 -0300

    Att - Layout and Icons in tiktok video

--- src/components/VideoTikoteko.js
+++ src/components/VideoTikoteko.js
@@ -1,13 +1,17 @@
 import video from "../video.mp4"
 import img from "../profile.jpeg";
-import { AiOutlineUserSwitch, AiFillHeart } from "react-icons/ai";
+import { AiOutlineUserSwitch, AiFillHeart, AiFillHome, AiOutlineSearch } from "react-icons/ai";
 import { FaRegCommentDots } from "react-icons/fa";
+import { HiPlusSm } from "react-icons/hi"
+import { MdOutlineChat } from "react-icons/md"
+import { IoArrowRedoOutline } from "react-icons/io5"
 
 const VideoTikoteko = (props) => {
     const playVideo = () => {
         let video = document.getElementById("video");
         video.play();
     }
+
     return(
         <div className='box-video'>
             <div className='title-video'>
@@ -18,18 +22,19 @@ const VideoTikoteko = (props) => {
                 <source src={video} type="video/mp4" />
             </video>
             <div className='icons-bottom'>
-                <AiOutlineUserSwitch style={{ fontSize:"30px", color: "white" }}></AiOutlineUserSwitch>
-                <AiOutlineUserSwitch style={{ fontSize:"30px", color: "white" }}></AiOutlineUserSwitch>
-                <AiOutlineUserSwitch style={{ fontSize:"30px", color: "white" }}></AiOutlineUserSwitch>
+                <AiFillHome style={{ fontSize:"30px", color: "white" }}></AiFillHome>
+                <AiOutlineSearch style={{ fontSize:"33px", color: "white" }}></AiOutlineSearch>
+                <HiPlusSm style={{ fontSize:"30px", color: "black", backgroundColor: "white", borderRadius: "10px", width: "50px" }}></HiPlusSm>
+                <MdOutlineChat style={{ fontSize:"29px", color: "white" }}></MdOutlineChat>
                 <AiOutlineUserSwitch style={{ fontSize:"30px", color: "white" }}></AiOutlineUserSwitch>
             </div>
             <div className='icons-right'>
                 <div className="profile">
                     <img src={img} width="100%" style={{borderRadius: "50%", width: "100%"}} />
                 </div>
-                <AiOutlineUserSwitch style={{ fontSize:"30px", color: "white", margin: "8px 0" }}></AiOutlineUserSwitch>
-                <AiFillHeart style={{ fontSize: "30px", color: "red", margin: "8px 0" }}></AiFillHeart>
-                <FaRegCommentDots style={{ fontSize: "30px", color: "white", margin: "8px 0" }}></FaRegCommentDots>
+                <AiFillHeart style={{ fontSize:"30px", color: "red", margin: "10px 0" }}></AiFillHeart>
+                <FaRegCommentDots style={{ fontSize: "30px", color: "white", margin: "15px 0" }}></FaRegCommentDots>
+                <IoArrowRedoOutline style={{ fontSize: "30px", color: "white", margin: "10px 0" }}></IoArrowRedoOutline>
             </div>
         </div>
     )
